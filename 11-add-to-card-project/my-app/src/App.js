2509521095950841270ea43f22812827b05133a5
Loliud <anhhaiquynhon123@gmail.com>
2019-07-13 22:00:48 +0700

    60% template

--- 11-add-to-card-project/my-app/src/App.js
+++ 11-add-to-card-project/my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Products from './components/Products';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-     
+      <Menu/>
+      <Products/>
+      <div id="goAhead"><a href="#menu"></a></div>
     </div>
   );
 }
