6ee39ec267bb977398827c444c4dbc58b519ef8e
Loliud <anhhaiquynhon123@gmail.com>
2019-07-13 21:54:59 +0700

    edit

--- 11-add-to-card-project/my-app/src/App.js
+++ 11-add-to-card-project/my-app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Menu from './components/Menu';
-import Products from './components/Products';
+
 function App() {
   return (
     <div className="App">
-        <Menu/>
-        <Products/>
-        <div id="goAhead"><a href="#menu"></a></div>
+     
     </div>
   );
 }
