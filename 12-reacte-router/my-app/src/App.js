9815c695e5fe1f5699e1cb50ef4c2659ff309e0a
Loliud <anhhaiquynhon123@gmail.com>
2019-07-16 21:17:26 +0700

    page is not found

--- 12-reacte-router/my-app/src/App.js
+++ 12-reacte-router/my-app/src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header';
 import About from './components/About.js';
 import Products from './components/Products.js';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Switch >
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/products" component={Products} />
+          <Route component={NotFound} />
+        </Switch>
+
+
       </div>
 
-      <Route path="/"  exact component={Home} />
-      <Route path="/about"  component={About} />
-      <Route path="/products"  component={Products} />
+
     </Router>
 
 
---
+++ 12-reacte-router/my-app/src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class NotFound extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+        return (
+            <div>
+                <h1>404 - PAGE IS NOT FOUND</h1>
+            </div>
+        )
+    }
+}
+
+
+export default NotFound;
