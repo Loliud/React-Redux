65210e6c232a6ccad8d97c0179539f76ccf37424
Loliud <anhhaiquynhon123@gmail.com>
2019-07-16 10:16:32 +0700

    complete add product, show message

--- 11-add-to-card-project/my-app/src/reducers/listBuys.js
+++ 11-add-to-card-project/my-app/src/reducers/listBuys.js
@@ -1,9 +1,16 @@
 import *as types from '../constants/ActionTypes';
-
+import *as messages from '../constants/Message';
 
 let data = JSON.parse(localStorage.getItem('listBuys'));
 let inititalState = data ? data : [];
 
+let checkNewProduct = (listBuys, newProduct) =>{
+    for(let i = 0; i < listBuys.length; i++){
+        if(listBuys[i].product.id === newProduct.product.id){
+            return true;
+        }
+    }
+}
 
 
 let myReducer = (state = inititalState, action) => {
@@ -13,8 +20,12 @@ let myReducer = (state = inititalState, action) => {
                 product: action.product,
                 quantity: action.quantity
             };
-            state.push(newProduct);
-            localStorage.setItem('listBuys' ,JSON.stringify(state));
+            if(checkNewProduct(state, newProduct) === true){
+               alert(messages.MSG_CARD_IS_ADDED);
+            }else{
+                state.push(newProduct);
+                localStorage.setItem('listBuys' ,JSON.stringify(state));
+            }
             return [...state];
         default:
             return [...state];
@@ -22,6 +33,8 @@ let myReducer = (state = inititalState, action) => {
 }
 
 export default myReducer;
+
+
 
 
 
