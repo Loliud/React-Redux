797363512807a874e73c812d6421075f0dc2a373
Loliud <anhhaiquynhon123@gmail.com>
2019-07-18 21:17:05 +0700

    complete template

--- 14-animal-store-project/my-app/src/App.js
+++ 14-animal-store-project/my-app/src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu/Menu';
+import { Toast, ToastBody, ToastHeader, Table, Button, Badge } from 'reactstrap';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        < Menu />
+        <Button color="warning">Thêm sản phẩm</Button>
+        <div className="p-3 bg-info my-2 rounded">
+          <Toast style={{ maxWidth: "100%", margin: "auto" }}>
+            <ToastHeader>
+              <h3>Danh sách sản phẩm</h3>
+            </ToastHeader  >
+            <ToastBody >
+              <Table bordered>
+                <thead>
+                  <tr>
+                    <th>STT</th>
+                    <th>Mã</th>
+                    <th>Tên sản phẩm</th>
+                    <th>Giá</th>
+                    <th>Trạng thái</th>
+                    <th>Hành động</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>3847384</td>
+                    <td>Mèo ấn độ</td>
+                    <td>1000 $</td>
+                    <td><Badge color="success">Còn hàng</Badge></td>
+                    <td><Button color="success">Sửa</Button><Button color="danger">Xóa</Button></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>3847384</td>
+                    <td>Mèo ấn độ</td>
+                    <td>1000 $</td>
+                    <td><Badge color="success">Còn hàng</Badge></td>
+                    <td><Button color="success">Sửa</Button><Button color="danger">Xóa</Button></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>3847384</td>
+                    <td>Mèo ấn độ</td>
+                    <td>1000 $</td>
+                    <td><Badge color="success">Còn hàng</Badge></td>
+                    <td><Button color="success">Sửa</Button><Button color="danger">Xóa</Button></td>
+                  </tr>
+                </tbody>
+              </Table>
+            </ToastBody>
+          </Toast>
+        </div>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
---
+++ 14-animal-store-project/my-app/src/components/Menu/Menu.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="red" light style={{backgroundColor: "#cecec8", marginBottom:"4rem"}}>
+          <NavbarBrand href="/" className="mr-auto" style={{color: "#617153"}}>S.Loliud Pet Store</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="#">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Products</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
